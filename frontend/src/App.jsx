adefd1e8cf9b07a6140b88abfcb58e051de9aeb4
teesha17 <teeshu.2179@gmail.com>
2024-04-16 19:12:44 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import {BrowserRouter as Router , Route,Routes} from "react-router-dom"
 import "./App.css"
+import Home from './screens/homepage/Home.jsx'
+import Contact from './screens/contact/Contact.jsx'
+import About from './screens/about/About.jsx'
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </Router>
   )
 }
